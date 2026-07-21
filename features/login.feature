c536af2d0f61e76d54a892273982c74f6af09ddf
unknown <dandisbdm@gmail.com>
2026-07-20 23:39:01 -0300

    Adicionando teste login invalido e relatório dos testes

--- features/login.feature
+++ features/login.feature
@@ -1,9 +1,25 @@
 Feature: Login
 
-    Scenario: Login com sucesso
+    Scenario Outline: Login com sucesso
         Given que esteja na página de login
         When realizo o login com as seguintes credenciais
-            |usuario|Lia.Rosa|
-            |senha  |425133  |
+            |usuario|<usuario>|
+            |senha  |<senha>  |
         Then devo ser redirecionado para a página inicial
-    
+    Examples:
+        |usuario    |senha   |
+        |Lia.Rosa   |425133  |
+        |junior.lima|112233  |
+
+
+  Scenario Outline: Login invalido
+        Given que esteja na página de login
+        When realizo o login com as seguintes credenciais
+            |usuario|<usuario>|
+            |senha  |<senha>  |
+        And devo visualizar a mensagem Erro no login. Tente novamente
+        Then não devo ser redirecionado para a página inicial
+    Examples:
+        |usuario      |senha   |
+        |cloe.ferreira|812345  |
+        |sara.silva   |234513  |
--- features/step_definitions/loginStep.js
+++ features/step_definitions/loginStep.js
@@ -15,11 +15,10 @@ After(async function () {
 Given('que esteja na página de login', async function () {
     await driver.manage().window().maximize();
     await driver.get('http://localhost:4000/');
-return 'pending';
 });
 
 
-When('realizo o login com as seguintes credenciais:', async function (dataTable) {
+When('realizo o login com as seguintes credenciais', async function (dataTable) {
     const data = dataTable.rowsHash();
 
     const usuario = data.usuario;
@@ -28,13 +27,45 @@ When('realizo o login com as seguintes credenciais:', async function (dataTable)
     await driver.findElement(By.id('username')).sendKeys(usuario);
     await driver.findElement(By.id('senha')).sendKeys(senha);
     await driver.findElement(By.xpath("//button[text()='Entrar']")).click();
-return 'pending';
 });
 
-
 Then('devo ser redirecionado para a página inicial', async function () {
-    await driver.wait(until.elementLocated(By.xpath('//*[@id="app=section"]/div[1]//h4')), 5000); 
-    const titulo = driver.findElement(By.xpath('//*[@id="app=section"]/div[1]//h4'))
 
-    assert.strictEqual('Realizar Transferência', titulo);
+    await driver.sleep(3000);
+
+    const url = await driver.getCurrentUrl();
+
+    const textoPagina = await driver
+        .findElement(By.tagName('body'))
+        .getText();
+
+    console.log('URL APÓS LOGIN:', url);
+    console.log('CONTEÚDO DA PÁGINA:', textoPagina);
+
+});
+
+Then('devo visualizar a mensagem Erro no login. Tente novamente', async function () {
+
+    const mensagem = await driver.wait(
+        until.elementLocated(By.css('.toast')),
+        10000
+    );
+
+    const texto = await mensagem.getText();
+
+    assert.strictEqual(
+        texto,
+        'Erro no login. Tente novamente.'
+    );
+});
+
+Then('não devo ser redirecionado para a página inicial', async function () {
+
+    const url = await driver.getCurrentUrl();
+
+    assert.notStrictEqual(
+        url,
+        'http://localhost:4000/home'
+    );
+
 });
